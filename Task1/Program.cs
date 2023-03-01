6e69bf01759b4fa95d35db3de9951f25a8d19835
Nikita Gritsanyuk <nikitagritcanuk@yandex.ru>
2023-03-01 14:29:47 +0800

    Add Task1-3

---
+++ Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿int digit = Convert.ToInt32(Console.ReadLine());
+int digit_tmp = digit / 10;
+digit_tmp = digit_tmp % 10;
+
+Console.WriteLine(Convert.ToString(digit) + " -> " + Convert.ToString(digit_tmp));
---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int digit = Convert.ToInt32(Console.ReadLine());
+int digit_tmp = digit;
+int i = 0;
+
+while(digit_tmp > 0) {
+    i++;
+    digit_tmp = digit_tmp / 10;
+}
+if(i == 3)
+    Console.WriteLine(Convert.ToString(digit) + " -> " + Convert.ToString(digit % 10));
+else
+    Console.WriteLine(Convert.ToString(digit) + " -> Такого числа нет");
